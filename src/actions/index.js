3088f6d19689e51a1371dc65a6278cae9ad9752a
Tristan Emmerson <tristan@stickerslug.com>
2020-09-26 16:31:10 -0700

    create index actions

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import * as c from "./ActionTypes";
+
+export const deleteSurvey = (id) => ({
+  type: c.DELETE_SURVEY,
+  id,
+});
+
+export const toggleForm = () => ({
+  type: c.TOGGLE_FORM,
+});
+
+export const updateTime = (id, formattedWaitTime) => ({
+  type: c.UPDATE_TIME,
+  id: id,
+  formattedWaitTime: formattedWaitTime,
+});
