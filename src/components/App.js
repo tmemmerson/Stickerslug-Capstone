0d2dd1bd40a3b8a91593b2511c2d75addf3af7f4
Tristan Emmerson <tristan@stickerslug.com>
2020-09-26 16:31:28 -0700

    update app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import SurveyControl from "./SurveyControl";
+import Signin from "./Signin";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+        <Route path="/">
+          <SurveyControl />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
