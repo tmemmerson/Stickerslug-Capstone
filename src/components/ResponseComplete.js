5c36f2bdc9a7c89d923d40938d9ba9c62e156276
Tristan Emmerson <tristan@stickerslug.com>
2020-09-26 16:33:31 -0700

    create response complete component

---
+++ src/components/ResponseComplete.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ResponseComplete() {
+  return (
+    <div>
+      <h1>Thank you for completing our survey!</h1>
+      <button>Return to survey list</button>
+      <button>Got to response list</button>
+    </div>
+  );
+}
+
+export default ResponseComplete;
