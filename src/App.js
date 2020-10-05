e5bdd5b915f0da4d88272b770a72c7bf8ce30c28
Tristan Emmerson <tristan@stickerslug.com>
2020-10-05 14:26:57 -0700

    remove compiler error resultant from favicon reference

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import logo from '../../public/favicon.ico';
-import './App.css';
-import Products from './components/Products';
-
+import React from "react";
+import "./App.css";
+import Products from "./components/Products";
 
 function App() {
   return (
     <div className="row">
       <div className="col-md-8 offset-md1">
-        < Products/>
+        <Products />
       </div>
     </div>
   );
