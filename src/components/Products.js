2c1ba74b9fb571aa3d25f25a56e1a57d4bc1bf3e
Tristan Emmerson <tristan@stickerslug.com>
2020-10-05 14:20:53 -0700

    write cursory table data fields to display to test functionality.

--- src/components/Products.js
+++ src/components/Products.js
@@ -36,4 +36,63 @@ const Products = () => {
       });
     }
   };
+
+  return (
+    <>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4 text-center">Product Manager</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-5">
+          <ProductForm
+            {...{ currentId, productObjects, addOrEdit }}
+          ></ProductForm>
+        </div>
+        <div className="col-md-7">
+          <table className="table table-borderless table-stripped">
+            <thead className="thead-light">
+              <tr>
+                <th>Serial</th>
+                <th>Oversized</th>
+                <th>size</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.keys(productObjects).map((key) => (
+                <tr key={key}>
+                  <td>{productObjects[key].serial}</td>
+                  <td>{productObjects[key].designAspectRatio}</td>
+                  <td>{productObjects.isoversized}</td>
+
+                  <td className="bg-light">
+                    <a
+                      className="btn text-primary"
+                      onClick={() => {
+                        setCurrentId(key);
+                      }}
+                    >
+                      <i className="fas fa-pencil-alt"></i>
+                    </a>
+                    <a
+                      className="btn text-danger"
+                      onClick={() => {
+                        onDelete(key);
+                      }}
+                    >
+                      <i className="far fa-trash-alt"></i>
+                    </a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 };
+
+export default Products;
