c9644ba3364f529dcbc081b1baadad841f6c2a98
Tristan Emmerson <tristan@stickerslug.com>
2020-10-05 13:48:55 -0700

    create sizeData product template

---
+++ src/components/ProductForm.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+const ProductForm = (props) => {
+  const initialFieldValues = {
+    serial: '',	
+    widthPixel: '',	
+    heightPixel: '',	
+    designOrientation: '',	
+    designAspectRatio: '',	
+    designAspectRatioOther: '',	
+    isOversized: '',	
+    fiveInchWidth: '',	
+    fiveInchHeight: '',	
+    eightInchWidth: '',	
+    eightInchHeight: '',	
+    elevenSWidth: '',	
+    elevenSHeight: '',	
+    fiveInchSizeName: '',	
+    eightInchSizeName: '',	
+    elevenSSizeName: '',	
+    thumbWidthPx: '',	
+    thumbHeightPx: ''
+  }
+
+  
+}
