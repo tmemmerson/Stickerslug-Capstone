a2ed40ab8878ae25a354ef2e1bd8bde4af016005
Tristan Emmerson <tristan@stickerslug.com>
2020-10-05 14:05:19 -0700

    add copies of input field syntax for each field in sizeData database

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,26 +1,354 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ProductForm = (props) => {
   const initialFieldValues = {
-    serial: '',	
-    widthPixel: '',	
-    heightPixel: '',	
-    designOrientation: '',	
-    designAspectRatio: '',	
-    designAspectRatioOther: '',	
-    isOversized: '',	
-    fiveInchWidth: '',	
-    fiveInchHeight: '',	
-    eightInchWidth: '',	
-    eightInchHeight: '',	
-    elevenSWidth: '',	
-    elevenSHeight: '',	
-    fiveInchSizeName: '',	
-    eightInchSizeName: '',	
-    elevenSSizeName: '',	
-    thumbWidthPx: '',	
-    thumbHeightPx: ''
-  }
+    serial: "",
+    widthPixel: "",
+    heightPixel: "",
+    designOrientation: "",
+    designAspectRatio: "",
+    designAspectRatioOther: "",
+    isOversized: "",
+    fiveInchWidth: "",
+    fiveInchHeight: "",
+    eightInchWidth: "",
+    eightInchHeight: "",
+    elevenSWidth: "",
+    elevenSHeight: "",
+    fiveInchSizeName: "",
+    eightInchSizeName: "",
+    elevenSSizeName: "",
+    thumbWidthPx: "",
+    thumbHeightPx: "",
+  };
+  var [values, setValues] = useState(initialFieldValues);
 
-  
-}
+  useEffect(() => {
+    if (props.currentId == "") setValues({ ...initialFieldValues });
+    else
+      setValues({
+        ...props.contactObjects[props.currentId],
+      });
+  }, [props.currentId, props.contactObjects]);
+
+  const handleInputChange = (e) => {
+    var { item, value } = e.target;
+    setValues({
+      ...values,
+      [item]: value,
+    });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    props.addOrEdit(values);
+  };
+  return (
+    <form autoComplete="off" onSubmit={handleFormSubmit}>
+      <div className="form-group input-group">
+        <div className="input-group-prepend">
+          <div className="input-group-text">
+            <i className="fas fa-user"></i>
+          </div>
+        </div>
+        <input
+          className="form-control"
+          name="fieldName"
+          placeholder="placeholder"
+          value={values.serial}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div className="form-row">
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="fieldName"
+            placeholder="placeholder"
+            value={values.serial}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="widthPixel"
+            placeholder="pixel width"
+            value={values.widthPixel}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="heightPixel"
+            placeholder="pixel height"
+            value={values.heightPixel}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="fieldName"
+            placeholder="placeholder"
+            value={values.serial}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="fieldName"
+            placeholder="placeholder"
+            value={values.serial}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="fieldName"
+            placeholder="placeholder"
+            value={values.serial}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="fieldName"
+            placeholder="placeholder"
+            value={values.serial}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="fieldName"
+            placeholder="placeholder"
+            value={values.serial}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="fieldName"
+            placeholder="placeholder"
+            value={values.serial}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="fieldName"
+            placeholder="placeholder"
+            value={values.serial}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="fieldName"
+            placeholder="placeholder"
+            value={values.serial}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="fieldName"
+            placeholder="placeholder"
+            value={values.serial}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="fieldName"
+            placeholder="placeholder"
+            value={values.serial}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="fieldName"
+            placeholder="placeholder"
+            value={values.serial}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="fieldName"
+            placeholder="placeholder"
+            value={values.serial}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="fieldName"
+            placeholder="placeholder"
+            value={values.serial}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="fieldName"
+            placeholder="placeholder"
+            value={values.serial}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group input-group col-md-6">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="fieldName"
+            placeholder="placeholder"
+            value={values.serial}
+            onChange={handleInputChange}
+          />
+        </div>
+      </div>
+      <div className="form-group">
+        /*instead of this input use textarea - issue with my syntax highlighter
+        */
+        <input
+          className="form-control"
+          name="address"
+          placeholder="Address"
+          value={values.address}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="form-group">
+        <input
+          type="submit"
+          value={props.currentId == "" ? "Save" : "Update"}
+          className="btn btn-primary btn-block"
+        />
+      </div>
+    </form>
+  );
+};
