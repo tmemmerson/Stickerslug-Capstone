fdc1531466a9e0ec499c3cf9900ad549347353a5
Tristan Emmerson <tristan@stickerslug.com>
2020-10-05 13:20:43 -0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
-import { createStore } from "redux";
-import rootReducer from "./reducers/index";
-import { Provider } from "react-redux";
-import { ReactReduxFirebaseProvider } from "react-redux-firebase";
-import { createFirestoreInstance } from "redux-firestore";
-import firebase from "./firebase";
-import "firebase/auth";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer);
-const rrfProps = {
-  firebase,
-  config: {
-    userProfile: "users",
-    useFirestoreForProfile: true,
-  },
-  dispatch: store.dispatch,
-  createFirestoreInstance,
-};
 ReactDOM.render(
-  <Provider store={store}>
-    <ReactReduxFirebaseProvider {...rrfProps}>
-      <App />
-    </ReactReduxFirebaseProvider>
-  </Provider>,
-  document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
