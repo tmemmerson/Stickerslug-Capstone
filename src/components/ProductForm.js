01a3e7dcfd354427d79f024444453a8195ca1639
Tristan Emmerson <tristan@stickerslug.com>
2020-10-06 12:25:31 -0700

    working

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -20,6 +20,30 @@ const ProductForm = (props) => {
     elevenSSizeName: "",
     thumbWidthPx: "",
     thumbHeightPx: "",
+    imgThmb: "",
+    baseName: "",
+    isSht: "",
+    isWin: "",
+    isMug: "",
+    isGym: "",
+    isOne: "",
+    isBag: "",
+    isVnk: "",
+    isCrw: "",
+    Rating: "",
+    aspectRatio: "",
+    imgSht: "",
+    imgWin: "",
+    imgMug: "",
+    imgGym: "",
+    imgOne: "",
+    imgBag: "",
+    imgVnk: "",
+    imgCrw: "",
+    imgGls: "",
+    imgMat: "",
+    imgCus: "",
+    needsEdit: "false",
   };
   var [values, setValues] = useState(initialFieldValues);
 
@@ -45,23 +69,8 @@ const ProductForm = (props) => {
   };
   return (
     <form autoComplete="off" onSubmit={handleFormSubmit}>
-      <div className="form-group input-group">
-        <div className="input-group-prepend">
-          <div className="input-group-text">
-            <i className="fas fa-user"></i>
-          </div>
-        </div>
-        <input
-          className="form-control"
-          name="fieldName"
-          placeholder="placeholder"
-          value={values.serial}
-          onChange={handleInputChange}
-        />
-      </div>
-
       <div className="form-row">
-        <div className="form-group input-group col-md-6">
+        <div className="form-group input-group col-md-10">
           <div className="input-group-prepend">
             <div className="input-group-text">
               <i className="fas fa-user"></i>
@@ -69,60 +78,161 @@ const ProductForm = (props) => {
           </div>
           <input
             className="form-control"
-            name="fieldName"
-            placeholder="placeholder"
-            value={values.serial}
+            name="baseName"
+            placeholder="title"
+            value={values.baseName}
+            onChange={handleInputChange}
+          />
+        </div>{" "}
+        <div className="form-group input-group col-md-2">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-exclamation-triangle"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="needsEdit"
+            placeholder="needsEdit"
+            value={values.needsEdit}
             onChange={handleInputChange}
           />
         </div>
-
-        <div className="form-group input-group col-md-6">
+      </div>
+      <div className="form-row">
+        <div className="form-group input-group col-md-3">
           <div className="input-group-prepend">
             <div className="input-group-text">
-              <i className="fas fa-user"></i>
+              <i className="fas fa-divide"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="aspectRatio"
+            placeholder="aspectRatio"
+            value={values.aspectRatio}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group input-group col-md-3">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-text-width"></i>
             </div>
           </div>
           <input
             className="form-control"
             name="widthPixel"
-            placeholder="pixel width"
+            placeholder="widthPixel"
             value={values.widthPixel}
             onChange={handleInputChange}
           />
         </div>
-      </div>
-      <div className="form-row">
-        <div className="form-group input-group col-md-6">
+        <div className="form-group input-group col-md-3">
           <div className="input-group-prepend">
             <div className="input-group-text">
-              <i className="fas fa-user"></i>
+              <i className="fas fa-text-height"></i>
             </div>
           </div>
           <input
             className="form-control"
             name="heightPixel"
-            placeholder="pixel height"
+            placeholder="heightPixel"
             value={values.heightPixel}
             onChange={handleInputChange}
           />
         </div>
-        <div className="form-group input-group col-md-6">
+      </div>
+      <div className="form-row">
+        <div className="form-group input-group col-md-2">
           <div className="input-group-prepend">
             <div className="input-group-text">
-              <i className="fas fa-user"></i>
+              <i className="fas fa-glass-whiskey"></i>
             </div>
           </div>
           <input
             className="form-control"
-            name="designOrientation"
-            placeholder="orientation"
-            value={values.designOrientation}
+            name="isSht"
+            placeholder="isSht"
+            value={values.isSht}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div className="form-group input-group col-md-2">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-wine-glass-alt"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="isWin"
+            placeholder="isWin"
+            value={values.isWin}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group input-group col-md-2">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-beer"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="isMug"
+            placeholder="isMug"
+            value={values.isMug}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group input-group col-md-2">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-baby"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="isOne"
+            placeholder="isOne"
+            value={values.isOne}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group input-group col-md-2">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-shopping-bag"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="isBag"
+            placeholder="isBag"
+            value={values.isBag}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group input-group col-md-2">
+          <div className="input-group-prepend">
+            <div className="input-group-text">
+              <i className="fas fa-dumbbell"></i>
+            </div>
+          </div>
+          <input
+            className="form-control"
+            name="isGym"
+            placeholder="isGym"
+            value={values.isGym}
             onChange={handleInputChange}
           />
         </div>
       </div>
       <div className="form-row">
-        <div className="form-group input-group col-md-6">
+        {" "}
+        <div className="form-group input-group col-md-2">
           <div className="input-group-prepend">
             <div className="input-group-text">
               <i className="fas fa-user"></i>
@@ -130,29 +240,27 @@ const ProductForm = (props) => {
           </div>
           <input
             className="form-control"
-            name="designAspectRatio"
-            placeholder="portrait ratio"
-            value={values.designAspectRatio}
+            name="isVnk"
+            placeholder="isVnk"
+            value={values.isVnk}
             onChange={handleInputChange}
           />
         </div>
-        <div className="form-group input-group col-md-6">
+        <div className="form-group input-group col-md-2">
           <div className="input-group-prepend">
             <div className="input-group-text">
-              <i className="fas fa-user"></i>
+              <i className="fas fa-tshirt"></i>
             </div>
           </div>
           <input
             className="form-control"
-            name="designAspectRatioOther"
-            placeholder="landscape ratio"
-            value={values.designAspectRatioOther}
+            name="isCrw"
+            placeholder="isCrw"
+            value={values.isCrw}
             onChange={handleInputChange}
           />
         </div>
-      </div>
-      <div className="form-row">
-        <div className="form-group input-group col-md-6">
+        <div className="form-group input-group col-md-2">
           <div className="input-group-prepend">
             <div className="input-group-text">
               <i className="fas fa-user"></i>
@@ -160,177 +268,15 @@ const ProductForm = (props) => {
           </div>
           <input
             className="form-control"
-            name="isOversized"
-            placeholder="Oversized?"
+            name="isOversied"
+            placeholder="isOversized"
             value={values.isOversized}
             onChange={handleInputChange}
           />
         </div>
-        <div className="form-group input-group col-md-6">
-          <div className="input-group-prepend">
-            <div className="input-group-text">
-              <i className="fas fa-user"></i>
-            </div>
-          </div>
-          <input
-            className="form-control"
-            name="fiveInchWidth"
-            placeholder="5in width"
-            value={values.fiveInchWidth}
-            onChange={handleInputChange}
-          />
-        </div>
       </div>
-      <div className="form-row">
-        <div className="form-group input-group col-md-6">
-          <div className="input-group-prepend">
-            <div className="input-group-text">
-              <i className="fas fa-user"></i>
-            </div>
-          </div>
-          <input
-            className="form-control"
-            name="fiveInchHeight"
-            placeholder="5in height"
-            value={values.fiveInchHeight}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-group input-group col-md-6">
-          <div className="input-group-prepend">
-            <div className="input-group-text">
-              <i className="fas fa-user"></i>
-            </div>
-          </div>
-          <input
-            className="form-control"
-            name="eightInchWidth"
-            placeholder="8in width"
-            value={values.eightInchWidth}
-            onChange={handleInputChange}
-          />
-        </div>
-      </div>
-      <div className="form-row">
-        <div className="form-group input-group col-md-6">
-          <div className="input-group-prepend">
-            <div className="input-group-text">
-              <i className="fas fa-user"></i>
-            </div>
-          </div>
-          <input
-            className="form-control"
-            name="eightInchHeight"
-            placeholder="8in height"
-            value={values.eightInchHeight}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-group input-group col-md-6">
-          <div className="input-group-prepend">
-            <div className="input-group-text">
-              <i className="fas fa-user"></i>
-            </div>
-          </div>
-          <input
-            className="form-control"
-            name="elevenSWidth"
-            placeholder="11s width"
-            value={values.elevenSWidth}
-            onChange={handleInputChange}
-          />
-        </div>
-      </div>
-      <div className="form-row">
-        <div className="form-group input-group col-md-6">
-          <div className="input-group-prepend">
-            <div className="input-group-text">
-              <i className="fas fa-user"></i>
-            </div>
-          </div>
-          <input
-            className="form-control"
-            name="elevenSHeight"
-            placeholder="11s height"
-            value={values.elevenSHeight}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-group input-group col-md-6">
-          <div className="input-group-prepend">
-            <div className="input-group-text">
-              <i className="fas fa-user"></i>
-            </div>
-          </div>
-          <input
-            className="form-control"
-            name="fiveInchSizeName"
-            placeholder="5in size name"
-            value={values.fiveInchSizeName}
-            onChange={handleInputChange}
-          />
-        </div>
-      </div>
-      <div className="form-row">
-        <div className="form-group input-group col-md-6">
-          <div className="input-group-prepend">
-            <div className="input-group-text">
-              <i className="fas fa-user"></i>
-            </div>
-          </div>
-          <input
-            className="form-control"
-            name="eightInchSizeName"
-            placeholder="8in size name"
-            value={values.eightInchSizeName}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-group input-group col-md-6">
-          <div className="input-group-prepend">
-            <div className="input-group-text">
-              <i className="fas fa-user"></i>
-            </div>
-          </div>
-          <input
-            className="form-control"
-            name="elevenSSizeName"
-            placeholder="11s size name"
-            value={values.elevenSSizeName}
-            onChange={handleInputChange}
-          />
-        </div>
-      </div>
-      <div className="form-row">
-        <div className="form-group input-group col-md-6">
-          <div className="input-group-prepend">
-            <div className="input-group-text">
-              <i className="fas fa-user"></i>
-            </div>
-          </div>
-          <input
-            className="form-control"
-            name="thumbWidthPx"
-            placeholder="thmb px width"
-            value={values.thumbWidthPx}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-group input-group col-md-6">
-          <div className="input-group-prepend">
-            <div className="input-group-text">
-              <i className="fas fa-user"></i>
-            </div>
-          </div>
-          <input
-            className="form-control"
-            name="thumbHeightPx"
-            placeholder="thmb px height"
-            value={values.thumbHeightPx}
-            onChange={handleInputChange}
-          />
-        </div>
-      </div>
+      <div className="form-row"></div>
+
       <div className="form-group">
         <input
           className="form-control"
@@ -343,7 +289,7 @@ const ProductForm = (props) => {
       <div className="form-group">
         <input
           type="submit"
-          value={props.currentId == "" ? "Save" : "Update"}
+          value={props.currentId === "" ? "Save" : "Update"}
           className="btn btn-primary btn-block"
         />
       </div>
--- src/components/Products.js
+++ src/components/Products.js
@@ -7,7 +7,7 @@ const Products = () => {
   var [productObjects, setProductObjects] = useState({});
 
   useEffect(() => {
-    firebaseDb.child("parentData").on("value", (snapshot) => {
+    firebaseDb.child("temp01").on("value", (snapshot) => {
       if (snapshot.val() != null) {
         setProductObjects({
           ...snapshot.val(),
@@ -18,19 +18,19 @@ const Products = () => {
 
   const addOrEdit = (obj) => {
     if (currentId == "")
-      firebaseDb.child("parentData").push(obj, (err) => {
+      firebaseDb.child("temp01").push(obj, (err) => {
         if (err) console.log(err);
         else setCurrentId("");
       });
     else
-      firebaseDb.child(`parentData/${currentId}`).set(obj, (err) => {
+      firebaseDb.child(`temp01/${currentId}`).set(obj, (err) => {
         if (err) console.log(err);
         else setCurrentId("");
       });
   };
   const onDelete = (id) => {
     if (window.confirm("Are you sure to delete this record?")) {
-      firebaseDb.child(`parentData/${id}`).remove((err) => {
+      firebaseDb.child(`temp01/${id}`).remove((err) => {
         if (err) console.log(err);
         else setCurrentId("");
       });
@@ -59,24 +59,24 @@ const Products = () => {
                 <th>Oversized</th>
                 <th>Image</th>
                 <th>Name</th>
-                <th>Action</th>
+                <th>baseName</th>
               </tr>
             </thead>
             <tbody>
               {Object.keys(productObjects).map((key) => (
                 <tr key={key}>
                   <td>{productObjects[key].serial}</td>
-                  <td>{productObjects[key].rating}</td>
-                  <td>{productObjects[key].isAGym}</td>
+                  <td>{productObjects[key].baseName}</td>
+                  <td>{productObjects[key].isGym}</td>
                   <td>
                     {" "}
                     <img
-                      src={productObjects[key].urlDGls}
+                      src={productObjects[key].imgThmb}
                       width={75}
-                      height={70}
+                      height={75}
                     />
                   </td>
-                  <td>{productObjects[key].name}</td>
+                  <td>{productObjects[key].baseName}</td>
 
                   <td className="bg-light">
                     <a
