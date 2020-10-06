41f8ea67903eb57a35db340183217d3a90bef92b
Tristan Emmerson <tristan@stickerslug.com>
2020-10-06 16:43:10 -0700

    remove error riddled nav bar from merge conflict and attempt to rebuild

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Products from "./components/Products";
-import NavBar from "./components/NavBar";
 
 function App() {
   return (
--- src/components/Session/index.js
+++
@@ -1,6 +0,0 @@
-import AuthUserContext, { withAuthContext } from "./context";
-import withAuthorization from "./withAuthorization";
-
-export default AuthUserContext;
-
-export { withAuthContext, withAuthorization };
