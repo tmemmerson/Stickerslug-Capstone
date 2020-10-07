ba505cc0f96e9c5d0661eec5457325fa8460a560
Tristan Emmerson <tristan@stickerslug.com>
2020-10-07 11:32:46 -0700

    create shell file for creating chart data for reports page

---
+++ src/components/data.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Stickerslug Capstone Product Chart</title>
+  </head>
+
+  <body>
+
+  </body>
+</html>
