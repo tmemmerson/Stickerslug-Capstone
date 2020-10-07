af1871c9631f25dcfed67671a358f7123c2a9708
Tristan Emmerson <tristan@stickerslug.com>
2020-10-07 11:33:41 -0700

    create component file for future splash page

---
+++ src/components/Splash.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import NavBar from "../layouts/DashboardLayout/NavBar";
+
+function Splash() {
+  return (
+    <React.Fragment>
+      <div className="row">
+        <div className="col-lg-3">
+          <NavBar />
+        </div>
+        <div className="col-lg-3">
+          <h4>header1</h4>
+          <p>body1</p>
+        </div>
+        <div className="col-lg-3">
+          <h4>header2</h4>
+          <p>body2</p>
+        </div>
+        <div className="col-lg-3">
+          <h4>header3</h4>
+          <p>body3</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-3"></div>
+        <div className="col-lg-3">
+          <h4>header1</h4>
+          <p>body1</p>
+        </div>
+        <div className="col-lg-3">
+          <h4>header2</h4>
+          <p>body2</p>
+        </div>
+        <div className="col-lg-3">
+          <h4>header3</h4>
+          <p>body3</p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Splash;
